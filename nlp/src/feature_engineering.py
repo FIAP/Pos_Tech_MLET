29a397ed51aaad5194cbdf19ba475fbf48ad3feb
Renata Gotler <renatagotler@gmail.com>
2024-07-23 20:22:16 +0000

    add feature engineering

---
+++ nlp/src/feature_engineering.py
@@ -0,0 +1,24 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+
+from src.preprocessing import TextPreprocessing
+
+
+class TextCleaner(BaseEstimator, TransformerMixin, TextPreprocessing):
+    def __init__(
+        self,
+        **kwargs
+    ):
+        """Initialize TextCleaner"""
+        self.kwargs = kwargs
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return X.apply(
+            lambda x:
+            self.preprocess_text(
+                text=x,
+                **self.kwargs
+            )
+        )
