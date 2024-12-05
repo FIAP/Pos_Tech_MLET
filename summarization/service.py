0fbd6f5a7196a05e43b5388c95656711be3045ea
Renata Gotler <renatagotler@gmail.com>
2024-12-05 18:06:08 +0000

    improve service

--- summarization/service.py
+++ summarization/service.py
@@ -1,10 +1,10 @@
 """Module responsible for serving with BentoML."""
 
 from __future__ import annotations
-
+from typing import List
 import bentoml
 
-EXAMPLE_INPUT = "Breaking News: In an astonishing turn of events, the small town of Willow Creek has been taken by storm as local resident Jerry Thompson's cat, Whiskers, performed what witnesses are calling a 'miraculous and gravity-defying leap.' Eyewitnesses report that Whiskers, an otherwise unremarkable tabby cat, jumped a record-breaking 20 feet into the air to catch a fly. The event, which took place in Thompson's backyard, is now being investigated by scientists for potential breaches in the laws of physics. Local authorities are considering a town festival to celebrate what is being hailed as 'The Leap of the Century.'"
+EXAMPLE_INPUT = ["Breaking News: In an astonishing turn of events, the small town of Willow Creek has been taken by storm as local resident Jerry Thompson's cat, Whiskers, performed what witnesses are calling a 'miraculous and gravity-defying leap.' Eyewitnesses report that Whiskers, an otherwise unremarkable tabby cat, jumped a record-breaking 20 feet into the air to catch a fly. The event, which took place in Thompson's backyard, is now being investigated by scientists for potential breaches in the laws of physics. Local authorities are considering a town festival to celebrate what is being hailed as 'The Leap of the Century.'"]
 
 
 @bentoml.service(
@@ -25,7 +25,7 @@ class Summarization:
         self.model = bentoml.mlflow.load_model(self.bento_model)
 
     @bentoml.api(batchable=True)
-    def summarize(self, texts: list[str] = EXAMPLE_INPUT) -> list[str]:
+    def summarize(self, texts: List[str] = EXAMPLE_INPUT) -> List[str]:
         """Summarize texts.
 
         Args:
@@ -38,5 +38,6 @@ class Summarization:
             mon.log(texts, name="request", role="input", data_type="list")
 
             summary_texts = self.model.predict(texts)
+            
             mon.log(summary_texts, name="response", role="prediction", data_type="list")
             return summary_texts
