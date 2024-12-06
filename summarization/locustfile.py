1296cb9bb44b98ac2d9077c6f57a9d9f40a5cf28
Renata Gotler <renatagotler@gmail.com>
2024-12-06 21:18:28 +0000

    add locust

---
+++ summarization/locustfile.py
@@ -0,0 +1,13 @@
+from locust import HttpUser
+from locust import between
+from locust import task
+
+from service import EXAMPLE_INPUT
+
+class SummarizerTestUser(HttpUser):
+    @task
+    def summarize(self):
+        url = "/summarize"
+        self.client.post(url, texts=EXAMPLE_INPUT)
+
+    wait_time = between(0.05, 2)
