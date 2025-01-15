13d5ac61e022f73d39992e4782aa81440a5b2e5a
Renata Gotler <renatagotler@gmail.com>
2025-01-15 09:27:24 -0300

    fix locust parameter

--- summarization/locustfile.py
+++ summarization/locustfile.py
@@ -9,6 +9,6 @@ class SummarizerTestUser(HttpUser):
     @task
     def summarize(self):
         url = "/summarize"
-        self.client.post(url, texts=EXAMPLE_INPUT)
+        self.client.post(url, json={"texts": EXAMPLE_INPUT})
 
     wait_time = between(0.05, 2)
