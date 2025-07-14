88596b02e3edcc522611fb3b084643a37a57e95b
iohpedro <91925850+iohpedro@users.noreply.github.com>
2025-07-13 22:52:42 -0300

    Add files via upload

---
+++ Fundamentos de Python ML/Aula 04 - Funções, Módulos e Boas práticas/calculadora_financeira/src/utilidades.py
@@ -0,0 +1,27 @@
+# utilidades.py
+
+def soma(a, b):
+    """
+    Retorna a soma de dois números.
+
+    Parâmetros:
+        a (int ou float): Primeiro número.
+        b (int ou float): Segundo número.
+
+    Retorna:
+        int ou float: A soma de a e b.
+    """
+    return a + b
+
+def multiplica(a, b):
+    """
+    Retorna o produto de dois números.
+
+    Parâmetros:
+        a (int ou float): Primeiro número.
+        b (int ou float): Segundo número.
+
+    Retorna:
+        int ou float: O produto de a e b.
+    """
+    return a * b
